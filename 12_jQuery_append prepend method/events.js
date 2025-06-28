e749d08fe95b2d5e2c6e58576d68a7004e98c747
Aditya Dubey <adityadubey793@gmail.com>
2025-06-28 20:48:55 +0530

    added append and prepend

---
+++ 12_jQuery_append prepend method/events.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("#handleAppend").click(function(){
+    $("#parent").append("<h2>cyberaditya</h2>");
+  });
+
+  $("#handlePrepend").click(function(){
+    $("#parent").prepend("<h2>cyberaditya</h2>");
+  });
+});
