b7d54a91cd4ef826a1c55d52957b377df33cae1f
Aditya Dubey <adityadubey793@gmail.com>
2025-06-28 20:54:25 +0530

    added after and before method

---
+++ 13_jQuery_after before method/events.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("#handleAfter").click(function(){
+    $("#parent").after("<h2>cyberaditya</h2>");
+  });
+
+  $("#handleBefore").click(function(){
+    $("#parent").before("<h2>cyberaditya</h2>");
+  });
+});
