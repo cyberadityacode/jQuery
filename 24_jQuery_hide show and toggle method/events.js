071a0b40e6d377b36313672538c3ea69728e61ef
Aditya Dubey <adityadubey793@gmail.com>
2025-06-29 23:00:33 +0530

    added hide show toggle

---
+++ 24_jQuery_hide show and toggle method/events.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#hide").click(function () {
+    $("#parent").hide();
+  });
+  $("#show").click(function () {
+    $("#parent").show();
+  });
+  $("#toggle").click(function () {
+    $("#parent").toggle();
+  });
+});
