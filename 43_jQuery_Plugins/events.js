d034d7d8987f42333492867e47fabd58aa40123d
Aditya Dubey <adityadubey793@gmail.com>
2025-06-30 14:51:21 +0530

    jquery plugin

---
+++ 43_jQuery_Plugins/events.js
@@ -0,0 +1,20 @@
+/* 
+  jQuery Plugins
+      ex. 1. map plugin download
+          2. call map plugin function
+          3. Pass parameter which location
+
+    drag and drop, accordian, chart, image effects etc
+
+  Steps:
+
+  1. Add jquery File in HTML page
+  2. Add Jquery plugin file
+  3. Attach Jquery plugin with HTML tag and pass parameter
+*/
+
+$(function () {
+  $.scrollUp({
+    scrollDistance: 50,
+  }); 
+});
