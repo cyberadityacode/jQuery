512cc7bd8fa854973a8af06b087e9f878f874b3b
Aditya Dubey <adityadubey793@gmail.com>
2025-06-30 14:13:14 +0530

    added event which

---
+++ 38_jQuery_event which/events.js
@@ -0,0 +1,10 @@
+/* 
+  event Type
+    
+*/
+
+$(document).ready(function () {
+  $("#result").on("click dblclick mouseover mouseout", function (event) {
+    $("h2").html("Event: " + event.which);
+  });
+});
